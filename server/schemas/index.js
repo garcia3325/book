a6a07351ea44b946d16f7af6a40640060f46541c
Rene Garcia Jr <renegarciajr.@Renes-Air.attlocal.net>
2022-08-07 20:20:05 -0400

    comit issue

---
+++ server/schemas/index.js
@@ -0,0 +1,5 @@
+// export the other two files
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+
+module.exports = { typeDefs, resolvers };
---
+++ server/server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const path = require('path');
+// const routes = require('./routes');
+
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+
+const { typeDefs, resolvers } = require('./schemas');
+
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+}
+
+// app.use(routes);
+
+
+
+const startApolloServer = async (typeDefs,resolvers) => {
+  await server.start();
+  server.applyMiddleware({app});
+  
+  db.once('open', () => {
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+    console.log(`Use GraphQL at 🚀 http://localhost:${PORT}${server.graphqlPath}`);
+  });
+
+}
+
+startApolloServer(typeDefs,resolvers);
